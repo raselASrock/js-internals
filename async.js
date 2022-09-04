35395ac65a2156cb1b1057e6bdba801bd45fe5e5
raselASrock <mdraselm413@gmail.com>
2022-09-04 23:18:36 +0600

    function added

---
+++ async.js
@@ -0,0 +1,15 @@
+// console.log('first');
+// console.log('second');
+// console.log('third');
+// console.log('forth');
+// console.log('fifth');
+
+
+function doSomething(){
+    console.log('third');
+}
+console.log('first');
+console.log('second');
+doSomething()
+console.log('forth');
+console.log('fifth');
