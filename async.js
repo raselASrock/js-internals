dfce0d9185c5cdc54908d99a872042448dde8f21
raselASrock <mdraselm413@gmail.com>
2022-09-04 23:27:04 +0600

    html file added

--- async.js
+++ async.js
@@ -5,11 +5,29 @@
 // console.log('fifth');
 
 
-function doSomething(){
-    console.log('third');
-}
+// function doSomething(){
+//     console.log('third');
+// }
+// console.log('first');
+// console.log('second');
+// doSomething()
+// console.log('forth');
+// console.log('fifth');
+
+
+// function doSomething(){
+//     console.log('third');
+// }
+// console.log('first');
+// console.log('second');
+// setTimeout(doSomething)
+// console.log('forth');
+// console.log('fifth');
+
+
+
 console.log('first');
 console.log('second');
-doSomething()
+setTimeout(() => {console.log('Third')}, 5000)
 console.log('forth');
 console.log('fifth');
