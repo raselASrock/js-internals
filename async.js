69eb7ce613e47126677ced5a621268f165aaba7d
raselASrock <mdraselm413@gmail.com>
2022-09-05 18:05:05 +0600

    code-added

--- async.js
+++ async.js
@@ -29,5 +29,8 @@
 console.log('first');
 console.log('second');
 setTimeout(() => {console.log('Third')}, 5000)
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => {console.log('response recieve'); return response.json()})
+  .then(json => console.log(json))
 console.log('forth');
 console.log('fifth');
