19b88a7e06896d97fbd9bd471823a20bc97b3a13
raselASrock <mdraselm413@gmail.com>
2022-09-05 18:58:15 +0600

    code

--- interval.js
+++ interval.js
@@ -1,5 +1,41 @@
+// console.log(`first`)
+// console.log(`second`)
+// // console.log(`third`)
+// setTimeout( ()=>{
+//     console.log('third');
+// })
+// console.log(`forth`)
+// console.log(`fifth`)
+
+
+// console.log(`first`)
+// console.log(`second`)
+// // console.log(`third`)
+// setInterval( ()=>{
+//     console.log('third');
+// })
+// console.log(`forth`)
+// console.log(`fifth`)
+
+
+// console.log(`first`)
+// console.log(`second`)
+// // console.log(`third`)
+// setInterval( ()=>{
+//     console.log('third');
+// }, 1000)
+// console.log(`forth`)
+// console.log(`fifth`)
+
+
 console.log(`first`)
 console.log(`second`)
-console.log(`third`)
+let seconds = 0;
+const intervalID = setInterval( ()=>{
+    console.log(seconds++);
+    if( seconds === 10 ){
+        clearInterval(intervalID)
+    }
+}, 1000)
 console.log(`forth`)
 console.log(`fifth`)
